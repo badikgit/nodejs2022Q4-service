5c98caee328c4386690a94ceb7ffab798ce22a94
badikgit <49131247+badikgit@users.noreply.github.com>
2023-02-13 00:49:23 +0300

    fix: change artist service, add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
@@ -8,6 +9,7 @@ const PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
 bootstrap();
