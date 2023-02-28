4eabee791b32f92d0a83a603577ebb588a0dc0f9
Stanislau Yarotski <stas_yar@tut.by>
2023-02-28 16:20:16 +0300

    fix: remove migrations, update package-lock

--- prisma/migrations/20230228083327_update_date_time3/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-  - You are about to alter the column `createdAt` on the `users` table. The data in that column could be lost. The data in that column will be cast from `Integer` to `BigInt`.
-  - You are about to alter the column `updatedAt` on the `users` table. The data in that column could be lost. The data in that column will be cast from `Integer` to `BigInt`.
-*/
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "createdAt" SET DATA TYPE BIGINT,
-ALTER COLUMN "updatedAt" SET DATA TYPE BIGINT;
