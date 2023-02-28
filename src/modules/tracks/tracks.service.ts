e0ef342b44df926bd4c0c7ecd37523a0bea2526d
Stanislau Yarotski <stas_yar@tut.by>
2023-02-28 03:45:49 +0300

    feat: modify tracks module according to prisma service

--- src/modules/tracks/tracks.service.ts
+++ src/modules/tracks/tracks.service.ts
@@ -1,29 +1,63 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTrackDto } from './dto/create-track.dto';
-import { UpdateTrackDto } from './dto/update-track.dto';
-import { DBService } from '../db/db.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateTrackDto, UpdateTrackDto } from './dto';
+import { Track } from '@prisma/client';
 
 @Injectable()
 export class TracksService {
-  constructor(private dbService: DBService) {}
+  constructor(private prisma: PrismaService) {}
 
-  async create(createTrackDto: CreateTrackDto) {
-    return await this.dbService.createTrack(createTrackDto);
+  async create(createTrackDto: CreateTrackDto): Promise<Track> {
+    return await this.prisma.track.create({
+      data: { ...createTrackDto },
+    });
   }
 
-  async findAll() {
-    return await this.dbService.getAllTracks();
+  async findAll(): Promise<Track[]> {
+    return await this.prisma.track.findMany();
   }
 
-  async findOne(id: string) {
-    return await this.dbService.getTrack(id);
+  async findOne(id: string): Promise<Track> {
+    return await this.prisma.track.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  async update(id: string, updateTrackDto: UpdateTrackDto) {
-    return await this.dbService.updateTrack(id, updateTrackDto);
+  async update(id: string, updateTrackDto: UpdateTrackDto): Promise<Track> {
+    const track = await this.prisma.track.findUnique({
+      where: {
+        id,
+      },
+    });
+    return this.prisma.track.update({
+      where: {
+        id,
+      },
+      data: {
+        ...track,
+        name: updateTrackDto.name ? updateTrackDto.name : track.name,
+        artistId:
+          updateTrackDto.artistId || updateTrackDto.artistId === null
+            ? updateTrackDto.artistId
+            : track.artistId,
+        albumId:
+          updateTrackDto.albumId || updateTrackDto.albumId === null
+            ? updateTrackDto.albumId
+            : track.albumId,
+        duration: updateTrackDto.duration
+          ? updateTrackDto.duration
+          : track.duration,
+      },
+    });
   }
 
-  async remove(id: string) {
-    return await this.dbService.removeTrack(id);
+  async remove(id: string): Promise<Track> {
+    return await this.prisma.track.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
