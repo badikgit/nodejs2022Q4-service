88cdc927bc30dbd83040aec46f0357374b7a59ab
Stanislau Yarotski <stas_yar@tut.by>
2023-03-05 00:12:20 +0300

    feat: update dependencies, add doc route, update Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { cwd } from 'process';
+import { parse } from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -10,6 +15,13 @@ const PORT = Number(process.env.PORT) || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+
+  const file = await readFile(join(cwd(), 'doc', 'api.yaml'), 'utf8');
+  const document = parse(file);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
