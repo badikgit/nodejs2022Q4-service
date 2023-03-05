0c4db2757934e83a91637e305897c13f16472131
Stanislau Yarotski <stas_yar@tut.by>
2023-03-05 11:38:43 +0300

    fix: fix LoggingService

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { appendFileSync, renameSync, statSync } from 'fs';
+import { appendFile, access, mkdir, open, rename, stat } from 'fs/promises';
 import { join } from 'path';
 import { cwd } from 'process';
 
@@ -14,27 +14,27 @@ export class LoggingService extends Logger {
     this.logLevel = Number(process.env.LOG_LEVEL) || 0;
   }
 
-  error(message: any, ...optionalParams: any[]) {
-    this.logMessage('error', 0, message, ...optionalParams);
+  async error(message: any, ...optionalParams: any[]) {
+    await this.logMessage('error', 0, message, ...optionalParams);
   }
 
-  warn(message: any, ...optionalParams: any[]) {
-    this.logMessage('warn', 1, message, ...optionalParams);
+  async warn(message: any, ...optionalParams: any[]) {
+    await this.logMessage('warn', 1, message, ...optionalParams);
   }
 
-  log(message: any, ...optionalParams: any[]) {
-    this.logMessage('log', 2, message, ...optionalParams);
+  async log(message: any, ...optionalParams: any[]) {
+    await this.logMessage('log', 2, message, ...optionalParams);
   }
 
-  debug(message: any, ...optionalParams: any[]) {
-    this.logMessage('debug', 3, message, ...optionalParams);
+  async debug(message: any, ...optionalParams: any[]) {
+    await this.logMessage('debug', 3, message, ...optionalParams);
   }
 
-  verbose(message: any, ...optionalParams: any[]) {
-    this.logMessage('verbose', 4, message, ...optionalParams);
+  async verbose(message: any, ...optionalParams: any[]) {
+    await this.logMessage('verbose', 4, message, ...optionalParams);
   }
 
-  logMessage(
+  async logMessage(
     logMethod: LogMethods,
     level: number,
     message: string,
@@ -43,43 +43,64 @@ export class LoggingService extends Logger {
     if (level > this.logLevel) return;
 
     super[logMethod](message, ...optionalParams);
-    this.writeToFile(logMethod, message);
+    await this.writeToFile(logMethod, message);
   }
 
-  writeToFile(level: LogMethods, message: string) {
-    const filePath = join(cwd(), 'logs', 'common-report.log');
-    const errorsFilePath = join(cwd(), 'logs', 'errors-report.log');
+  async writeToFile(level: LogMethods, message: string) {
+    const commonReportPath = join(cwd(), 'logs', 'common-report.log');
+    const errorsReportPath = join(cwd(), 'logs', 'errors-report.log');
+    try {
+      await access(join(cwd(), 'logs'));
+    } catch {
+      await mkdir('logs');
+    }
+    try {
+      await access(commonReportPath);
+    } catch {
+      await open(commonReportPath, 'w');
+    }
+    try {
+      await access(errorsReportPath);
+    } catch {
+      await open(errorsReportPath, 'w');
+    }
 
-    const maxSize = Number(process.env.MAX_LOG_FILE_SIZE) || 1000;
+    const maxSize = Number(process.env.MAX_LOG_FILE_SIZE) || 10;
 
     try {
-      const stats = statSync(filePath);
+      const stats = await stat(commonReportPath);
 
       if (stats.size > maxSize * 1024) {
-        renameSync(
-          filePath,
-          filePath.replace(/.log$/, `-${new Date().getTime()}-old.log`),
+        await rename(
+          commonReportPath,
+          commonReportPath.replace(/.log$/, `-${new Date().getTime()}-old.log`),
         );
       }
     } catch (err) {}
 
     const time = new Date().toLocaleString();
-    appendFileSync(filePath, `${time}   ${level.toUpperCase()} - ${message}\n`);
+    await appendFile(
+      commonReportPath,
+      `${time}   ${level.toUpperCase()} - ${message}\n`,
+    );
 
     if (level === 'error') {
       try {
-        const errorsStats = statSync(errorsFilePath);
+        const errorsStats = await stat(errorsReportPath);
 
         if (errorsStats.size > maxSize * 1024) {
-          renameSync(
-            errorsFilePath,
-            errorsFilePath.replace(/.log$/, `-${new Date().getTime()}-old.log`),
+          await rename(
+            errorsReportPath,
+            errorsReportPath.replace(
+              /.log$/,
+              `-${new Date().getTime()}-old.log`,
+            ),
           );
         }
       } catch (err) {}
 
-      appendFileSync(
-        errorsFilePath,
+      await appendFile(
+        errorsReportPath,
         `${time}   ${level.toUpperCase()} - ${message}\n`,
       );
     }
