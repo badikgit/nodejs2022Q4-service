a75df747b509c7384937fd31dd2b2d27f63a2235
Stanislau Yarotski <stas_yar@tut.by>
2023-03-05 06:58:46 +0300

    feat: create and add http excaption filter, add logging for uncaughtException and unhandledRejection

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
-import { cwd } from 'process';
+import { cwd, exit } from 'process';
 import { parse } from 'yaml';
 import { SwaggerModule } from '@nestjs/swagger';
+import { LoggingService } from './logging/logging.service';
+import { HttpExceptionFilter } from './logging/http-exeption.filter';
 
 dotenv.config();
 
@@ -14,6 +16,10 @@ const PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = app.get(LoggingService);
+  app.useGlobalFilters(new HttpExceptionFilter(logger));
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
 
@@ -23,5 +29,14 @@ async function bootstrap() {
   SwaggerModule.setup('doc', app, document);
 
   await app.listen(PORT);
+
+  process.on('uncaughtException', (err) => {
+    logger.error(`Uncaught Exception thrown: ${err}`);
+    exit(1);
+  });
+
+  process.on('unhandledRejection', (reason) => {
+    logger.error(`Unhandled Rejection at Promise with reason: ${reason}`);
+  });
 }
 bootstrap();
